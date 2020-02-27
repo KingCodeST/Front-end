356ccde8522961b15c7006fe5733e4efb44d379e
Siphokuhle <cyfotyasi@gmail.com>
2020-02-28 01:28:58 +0200

    myne

---
+++ src/app/controller/user.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../domain/user';
+@Component({
+    selector: 'app-user',
+    templateUrl:'./user.componet.html',
+    styleUrls: ['./user.componet.css']
+})
+
+export class UserComponent{
+
+
+}
