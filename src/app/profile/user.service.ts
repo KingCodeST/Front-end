a1fc81f0434b50f19549341d387ced6f78a835b1
Siphokuhle <cyfotyasi@gmail.com>
2020-07-19 22:01:12 +0200

    git .

--- src/app/profile/user.service.ts
+++ src/app/profile/user.service.ts
@@ -1,4 +1,4 @@
-import {Http, Response, RequestOptions, Headers } from '@angular/common/';
+import { HttpHeaders } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../shuttle/user.model';
@@ -6,20 +6,30 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch'
 
 
-@Injectable()
-export class UserService{
-constructor(private _httpService: Http){}
+//@Injectable()
+// export class UserService{
+// //constructor(private _httpService: Http){}
 
-getall(): Observable<User[]>{
-  return this._httpService.get("http://localhost:8080/user")
-    .map((response:Response)=> response.json())
-    .catch(this.handlerError);
-}
+// // getall(): Observable<User[]>{
+// //   return this._httpService.get("http://localhost:8080/user")
+// //     .map((response:Response)=> response.json())
+// //     .catch(this.handlerError);
+// // }
 
-private handlerError(error: Response)
-{
-    return Observable.throw(error);
+// // private handlerError(error: Response)
+// // {
+// //     return Observable.throw(error);
     
-}
+// // }
 
-}
+
+// }
+
+
+const httpOptions ={
+    headers: new HttpHeaders({
+       'Content-Type': 'application/json',
+       'Authorization': 'my-auth-token' 
+    })
+};
+
